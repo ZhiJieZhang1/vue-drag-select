9d680fa4add746c2f78ce11d31d67d90905218cd
zhangzhijie <15620624311@163.com>
2018-03-07 17:06:15 +0800

    example

---
+++ example/dragSelect/src/components/DragSelect.vue
@@ -0,0 +1,412 @@
+<template>
+  <div class="vue-drag-select">
+    <div class="select-wrap" @mousedown="onMouseDown" :style="{minHeight: minHeight + 'px'}">
+      <div class="wrap-container" ref="selectWrap">
+        <slot>
+        </slot>
+      </div>
+      <div
+        v-if="mouseDown"
+        class="vue-drag-select-box"
+        :style="selectionBoxStyling">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-drag-select',
+  props: {
+    value: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    minHeight: {
+      type: Number,
+      required: true
+    },
+    dataList: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      // 鼠标按下状态
+      mouseDown: false,
+      // 起始点
+      startPoint: null,
+      // 结束点
+      endPoint: null,
+      // 最终计算 结束点
+      lastEndPoint: null,
+      // 滚动状态
+      scrollIng: false,
+      // 自动滚动计时器
+      autoScrollTimer: null,
+      // 滚动方向 true 向下 false 向上
+      scrollDirection: true,
+      // 自动滚动速度
+      scrollSpeed: 20,
+      // 选框最终数据
+      selectionBox: {
+        left: 0,
+        top: 0,
+        width: 0,
+        height: 0
+      },
+      // 选框最终样式
+      selectionBoxStyling: {
+        left: '0px',
+        top: '0px',
+        width: '0px',
+        height: '0px'
+      },
+      // 按键状态
+      controlKeyDown: false,
+      shiftKeyDown: false
+    }
+  },
+  computed: {
+    currentValue: {
+      get: function () {
+        return this.value
+      },
+      set: function (val) {
+        this.$emit('input', val)
+      }
+    }
+  },
+  methods: {
+    boxAutoScroll () {
+      const self = this
+      self.scrollIng = true
+      self.autoScrollTimer = setInterval(() => {
+        if (!self.mouseDown) {
+          clearInterval(self.autoScrollTimer)
+          return
+        }
+        if (self.scrollDirection) {
+          if ((self.$el.scrollTop + self.$el.offsetHeight) >= (self.minHeight - 20)) {
+            clearInterval(self.autoScrollTimer)
+            self.scrollIng = false
+          } else {
+            self.$el.scrollTop += self.scrollSpeed
+          }
+        } else {
+          if (self.$el.scrollTop <= 0) {
+            clearInterval(self.autoScrollTimer)
+            self.scrollIng = false
+          } else {
+            self.$el.scrollTop -= self.scrollSpeed
+          }
+        }
+      }, 16)
+    },
+    boxScrollFn (event) {
+      this.computeLastEndPoint()
+    },
+    computeLastEndPoint () {
+      if (!this.mouseDown || !this.startPoint || !this.endPoint) return {}
+      const clientRect = this.$el.getBoundingClientRect()
+      this.lastEndPoint = {
+        x: this.endPoint.x - clientRect.left,
+        y: this.endPoint.y - clientRect.top + this.$el.scrollTop
+      }
+      // Calculate position and dimensions of the selection box
+      const left = Math.min(this.startPoint.x, this.lastEndPoint.x)
+      const top = Math.min(this.startPoint.y, this.lastEndPoint.y)
+      const width = Math.abs(this.startPoint.x - this.lastEndPoint.x)
+      const height = Math.abs(this.startPoint.y - this.lastEndPoint.y)
+      this.selectionBox = {
+        left,
+        top,
+        width,
+        height
+      }
+      this.selectionBoxStyling = {
+        left: `${left}px`,
+        top: `${top}px`,
+        width: `${width}px`,
+        height: `${height}px`
+      }
+    },
+    onMouseDown (event) {
+      // 阻止捕获到item本身
+      if (!((event.target.className === 'select-wrap') || (event.target.className === 'box-item'))) return
+      // Ignore right clicks
+      if (event.button === 2) return
+      const clientRect = this.$el.getBoundingClientRect()
+      // Register begin point
+      this.mouseDown = true
+      this.startPoint = {
+        x: event.pageX - clientRect.left,
+        y: event.pageY - clientRect.top + this.$el.scrollTop
+      }
+      // Start listening for mouse move and up events
+      window.addEventListener('mousemove', this.onMouseMove)
+      window.addEventListener('mouseup', this.onMouseUp)
+    },
+    onMouseMove (event) {
+      if (!this.mouseDown) return
+      const clientRect = this.$el.getBoundingClientRect()
+      // 外容器底部到client顶部的距离
+      const elBottom = clientRect.top + clientRect.height
+
+      // this.endPoint.y = event.pageY
+      if (event.pageY > (elBottom - 50)) {
+        // 向下滚动
+        this.scrollDirection = true
+      }
+
+      if (event.pageY < (clientRect.top + 50)) {
+        // 向上滚动
+        this.scrollDirection = false
+      }
+
+      // 判断是否开启滚动
+      if ((event.pageY > (clientRect.top + 50)) && (event.pageY < (elBottom - 50))) {
+        clearInterval(this.autoScrollTimer)
+        this.scrollIng = false
+      } else {
+        // 可以开启滚动时，只开启一次滚动
+        if (!this.scrollIng) {
+          this.boxAutoScroll()
+        }
+        this.scrollSpeed = 20
+      }
+      if ((event.pageY < (clientRect.top)) || (event.pageY > elBottom)) {
+        this.scrollSpeed = 50
+      }
+      this.endPoint = {
+        x: event.pageX,
+        y: event.pageY
+      }
+      if (this.scrollIng) {
+
+      } else {
+        this.computeLastEndPoint()
+      }
+      // this.computeLastEndPoint()
+    },
+    onMouseUp (event) {
+      // Clean up event listeners
+      window.removeEventListener('mousemove', this.onMouseMove)
+      window.removeEventListener('mouseup', this.onMouseUp)
+      // Reset state
+      this.mouseDown = false
+      this.computeSelectedItems()
+      this.startPoint = null
+      this.endPoint = null
+      this.lastEndPoint = null
+      this.selectionBox = {
+        left: 0,
+        top: 0,
+        width: 0,
+        height: 0
+      }
+      this.selectionBoxStyling = {
+        left: '0px',
+        top: '0px',
+        width: '0px',
+        height: '0px'
+      }
+    },
+    computeSelectedItems () {
+      const self = this
+      if (!this.endPoint || Math.abs(this.endPoint.x - this.startPoint.x) < 2 || Math.abs(this.endPoint.y - this.startPoint.y) < 2) {
+        this.currentValue = []
+        return
+      }
+      Array.from(self.$refs.selectWrap.children).forEach((item) => {
+        // debugger
+        const box = {
+          top: item.offsetTop,
+          left: item.offsetLeft,
+          width: item.clientWidth,
+          height: item.clientHeight
+        }
+        const isChecked =
+        self.selectionBox.left <= box.left + box.width &&
+        self.selectionBox.left + self.selectionBox.width >= box.left &&
+        self.selectionBox.top <= box.top + box.height &&
+        self.selectionBox.top + self.selectionBox.height >= box.top
+        // debugger
+        if (isChecked) {
+          if (self.currentValue.indexOf(item.id) !== -1) {
+            self.currentValue.splice(self.currentValue.indexOf(item.id), 1)
+          } else {
+            // debugger
+            self.currentValue.push(item.id)
+          }
+        }
+      })
+    },
+    clickSelect (val, indexVal) {
+      if (this.shiftKeyDown) {
+        // debugger
+        const indexArr = []
+        const self = this
+        const slelectedListLength = self.currentValue.length
+        self.dataList.forEach((item, index) => {
+          // debugger
+          if (self.currentValue.indexOf(item.id) !== -1) {
+            indexArr.push(index)
+          }
+        })
+        self.currentValue = []
+        self.$nextTick(() => {
+          if (slelectedListLength === 0) {
+            self.dataList.forEach((item, index) => {
+              if (index <= indexVal) {
+                self.currentValue.push(item.id)
+              }
+            })
+          } else if (slelectedListLength === 1) {
+            if (indexArr[0] > indexVal) {
+              self.dataList.forEach((item, index) => {
+                // debugger
+                if (index >= indexVal && index <= indexArr[0]) {
+                  self.currentValue.push(item.id)
+                }
+              })
+            } else {
+              self.dataList.forEach((item, index) => {
+                if (index <= indexVal && index >= indexArr[0]) {
+                  self.currentValue.push(item.id)
+                }
+              })
+            }
+          } else if (slelectedListLength >= 2) {
+            const maxIndex = Math.max.apply(null, indexArr)
+            const minIndex = Math.min.apply(null, indexArr)
+            if (indexVal >= maxIndex) {
+              self.dataList.forEach((item, index) => {
+                if (index <= indexVal && index >= maxIndex) {
+                  self.currentValue.push(item.id)
+                }
+              })
+            } else if (indexVal >= minIndex && indexVal < maxIndex) {
+              const nearMinArr = []
+              indexArr.forEach((item) => {
+                if (item >= minIndex && item <= indexVal) {
+                  nearMinArr.push(item)
+                }
+              })
+              const nearMinIndex = Math.max.apply(null, nearMinArr)
+              self.dataList.forEach((item, index) => {
+                if (index >= nearMinIndex && index <= indexVal) {
+                  self.currentValue.push(item.id)
+                }
+              })
+            } else {
+              self.dataList.forEach((item, index) => {
+                if (index >= indexVal && index <= minIndex) {
+                  self.currentValue.push(item.id)
+                }
+              })
+            }
+          }
+        })
+        return
+      }
+      if (!this.controlKeyDown) {
+        this.currentValue = []
+        this.$nextTick(() => {
+          this.currentValue.push(val)
+        })
+      } else {
+        if (this.currentValue.indexOf(val) !== -1) {
+          this.currentValue.splice(this.currentValue.indexOf(val), 1)
+        } else {
+          this.currentValue.push(val)
+        }
+      }
+    },
+    // 监听按键
+    detectKey () {
+      const isWin = navigator.userAgent.indexOf('Window') > 0
+      const isMac = navigator.userAgent.indexOf('Mac OS X') > 0
+      const self = this
+      // 按  Shift_L  Control_L  command
+      window.onkeydown = function (event) {
+        // debugger
+        const e = event || window.event
+        if (e && e.keyCode === 16) {
+          // debugger
+          self.shiftKeyDown = true
+        }
+        if (e && e.keyCode === 17 && isWin) {
+          // debugger
+          self.controlKeyDown = true
+        }
+        if (e && e.keyCode === 91 && isMac) {
+          // debugger
+          self.controlKeyDown = true
+        }
+      }
+      // 放 Shift_L  Control_L  command
+      window.onkeyup = function (event) {
+        const e = event || window.event
+        if (e && e.keyCode === 16) {
+          // debugger
+          self.shiftKeyDown = false
+        }
+        if (e && e.keyCode === 17 && isWin) {
+          // debugger
+          self.controlKeyDown = false
+        }
+        if (e && e.keyCode === 91 && isMac) {
+          // debugger
+          self.controlKeyDown = false
+        }
+      }
+    }
+  },
+  mounted () {
+    this.$el.addEventListener('scroll', this.boxScrollFn)
+    this.detectKey()
+  },
+  beforeDestroy () {
+    // Remove event listeners
+    window.removeEventListener('mousemove', this.onMouseMove)
+    window.removeEventListener('mouseup', this.onMouseUp)
+    this.$el.removeEventListener('scroll', this.boxScrollFn)
+  }
+}
+</script>
+
+<style lang="less">
+  .vue-drag-select {
+    position: relative;
+    user-select: none;
+    overflow:hidden;
+    width:100%;
+    height:100%;
+    overflow-y: visible;
+    will-change:transform;
+    transform:translate3d(0,0,0);
+    .select-wrap{
+      position: relative;
+      user-select: none;
+      padding-left:10px;
+      min-width:100%;
+      min-height:100%;
+      .wrap-container{
+        width:100%;
+        height:100%;
+      }
+    }
+  }
+  .vue-drag-select-box {
+    position: absolute;
+    background: rgba(0, 162, 255, .4);
+    z-index: 99;
+    border:1px dotted rgb(0, 162, 255);
+  }
+</style>
---
+++ example/dragSelect/src/components/ParentCom.vue
@@ -0,0 +1,190 @@
+<template>
+  <div :class="['index-page', mood ? '' : 'bigger']">
+    <div class="btn" @click="mood = !mood">{{mood ? '点击变宽' : '点击收缩'}}</div>
+    <div class="tips">可以尝试 <code>Ctrl</code> 和 <code>shift</code> 按键</div>
+    <div ref="resultBox" class="result-box">
+        <drag-select :min-height="minHeight" v-model="selectedList" :data-list="dataList" ref="dragSelect">
+          <template v-for="(item, index) in dataList">
+            <div :class="['box-item', item.selected ? 'selected-item' : '']" :key="item.id" :id="item.id" @click="clickItem(item.id, index)">
+              <div class="item-self">
+                <img v-if="index !== 31" src="../assets/images/0.jpg">
+                <img v-else src="../assets/images/prank.jpg">
+              </div>
+            </div>
+          </template>
+        </drag-select>
+      </div>
+  </div>
+</template>
+
+<script>
+import DragSelect from './DragSelect'
+
+export default {
+  components: {
+    DragSelect
+  },
+  data () {
+    return {
+      mood: true,
+      minHeight: 0,
+      dataList: [
+        {
+          id: '1'
+        },
+        {
+          id: '2'
+        },
+        {
+          id: '3'
+        }
+      ],
+      selectedList: []
+    }
+  },
+  methods: {
+    siteHandle (itemWidth, itemHeight) {
+      // 容器宽度
+      const boxWidth = this.$refs.resultBox.offsetWidth
+      // 一行几个
+      const colCount = parseInt(boxWidth / itemWidth)
+      // 容器内全部元素 数组
+      const domArr = this.$refs.resultBox.querySelectorAll('.box-item')
+      let vtSite = 0
+      // debugger;
+      domArr.forEach((item, index) => {
+        // 换行
+        // debugger;
+        if (!(index % colCount)) {
+          if (index) {
+            vtSite += 1
+          }
+        }
+        // item.style.transform = `translate(${(index % colCount) * itemWidth}px,${vtSite * itemHeight}px)`
+        item.style.top = `${vtSite * itemHeight}px`
+        item.style.left = `${(index % colCount) * itemWidth}px`
+      })
+      this.minHeight = (vtSite + 1) * itemHeight
+    },
+    clickItem (val, indexVal) {
+      this.$refs.dragSelect.clickSelect(val, indexVal)
+    }
+  },
+  created () {
+    this.dataList = []
+    for (let i = 1; i <= 50; i++) {
+      this.dataList.push({
+        id: `${i}`
+      })
+    }
+  },
+  mounted () {
+    this.siteHandle(300, 340)
+  },
+  watch: {
+    mood () {
+      // debugger;
+      clearTimeout(this.timeClick)
+      this.timeClick = setTimeout(() => {
+        this.siteHandle(300, 340)
+      }, 500)
+    },
+    selectedList (val) {
+      // console.log(val)
+      this.dataList.forEach((item) => {
+        if (val.indexOf(item.id) !== -1) {
+          item.selected = true
+        } else {
+          item.selected = false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.index-page {
+  width:100%;
+  height:100%;
+  position:relative;
+  padding:0 0 0 400px;
+  transition:all .3s ease;
+  .btn{
+    padding:10px 20px;
+    color:#fff;
+    position:absolute;
+    left:10px;
+    top:50px;
+    background-color:#2d8cf0;
+    border-radius:4px;
+    cursor:pointer;
+    transition:all .3s ease;
+    user-select: none;
+    &:hover{
+      background-color:#57a3f3;
+    }
+    &:focus{
+      box-shadow:0 0 0 2px rgba(45,140,240,.2);
+    }
+    &:active{
+      background-color:#2d8cf0;
+    }
+  }
+  .tips{
+    position:absolute;
+    left:10px;
+    top:150px;
+    code{
+      background:#000;
+      color:#fff;
+      padding:10px;
+      border-radius:6px;
+    }
+  }
+  .result-box{
+    width:100%;
+    height:100%;
+    // background-color:#f4f7fa;
+    transition:all .3s ease;
+    display:flex;
+    flex-wrap: wrap;
+    position:relative;
+    overflow-x: hidden;
+    overflow-y: visible;
+    .box-item{
+      width:280px;
+      height:300px;
+      margin: 20px;
+      transition:all .6s ease;
+      position:absolute;
+      .item-self{
+        width:100%;
+        height:100%;
+        border-radius:4px;
+        border:1px solid #fff;
+        background-color:#fff;
+        transition: all .3s ease;
+        overflow:hidden;
+        &:hover{
+          box-shadow:0px 2px 20px -2px rgba(0,0,0,.5);
+        }
+        img{
+          width:100%;
+        }
+      }
+    }
+    .selected-item{
+      .item-self{
+        border:1px solid red;
+        border-color: rgb(65, 98, 255);
+        box-shadow: rgb(65, 98, 255) 0px 0px 0px 2px !important;
+      }
+    }
+  }
+}
+.bigger{
+  width:100%;
+  padding:0 0 0 100px;
+}
+</style>
