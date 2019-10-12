b372745486ddf5ccde4d64a7765db12d06bf0720
JanssenZhang <zhangzhijay@gmail.com>
2019-10-12 17:07:25 +0800

    update

--- examples/components/ParentCom.vue
+++ examples/components/ParentCom.vue
@@ -3,28 +3,37 @@
     <div class="tool-box">
       <div class="btn" @click="mood = !mood">{{mood ? '点击变宽' : '点击收缩'}}</div>
       <div class="btn" @click="insertItem">添加一个皮卡丘</div>
-      <div class="tips">可以尝试 <code>Ctrl</code>、<code>command</code> 和 <code>shift</code> 按键</div>
+      <div class="tips">
+        可以尝试
+        <code>Ctrl</code>、
+        <code>command</code> 和
+        <code>shift</code> 按键
+      </div>
     </div>
     <div class="result-box">
-        <vue-drag-select v-model="selectedList" value-key="name" :item-margin="[0, 10, 10, 0]" ref="dragSelect">
-          <template v-for="(item, index) in dataList">
-            <drag-select-option :key="item.id" :value="item" :item-index="index">
-              <div class="item-self">
-                <img v-if="index === 100" src="../assets/images/prank.jpg">
-                <img v-else-if="item.lip" src="../assets/images/1.jpg">
-                <img v-else src="../assets/images/0.jpg">
-              </div>
-            </drag-select-option>
-          </template>
-        </vue-drag-select>
-      </div>
+      <vue-drag-select
+        v-model="selectedList"
+        value-key="name"
+        :item-margin="[0, 10, 10, 0]"
+        ref="dragSelect"
+      >
+        <template v-for="(item, index) in dataList">
+          <drag-select-option :key="item.id" :value="item" :item-index="index">
+            <div class="item-self">
+              <img v-if="index === 100" src="../assets/images/prank.jpg" />
+              <img v-else-if="item.lip" src="../assets/images/1.jpg" />
+              <img v-else src="../assets/images/0.jpg" />
+            </div>
+          </drag-select-option>
+        </template>
+      </vue-drag-select>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  data () {
+  data() {
     return {
       mood: true,
       dataList: [
@@ -38,88 +47,91 @@ export default {
           id: 3
         }
       ],
-      selectedList: []
-    }
+      selectedList: [],
+      id: 300
+    };
   },
-  created () {
-    this.dataList = []
+  created() {
+    this.dataList = [];
     for (let i = 50; i <= 300; i++) {
       this.dataList.push({
         id: i,
         name: i
-      })
+      });
     }
   },
   watch: {
-    mood () {
-      clearTimeout(this.timeClick)
+    mood() {
+      clearTimeout(this.timeClick);
       this.timeClick = setTimeout(() => {
-        this.$refs.dragSelect.elementLayout(200, 230)
-      }, 500)
+        this.$refs.dragSelect.elementLayout(200, 230);
+      }, 500);
     }
   },
 
   methods: {
-    insertItem () {
-      const { id, name } = this.dataList[this.dataList.length - 1]
+    insertItem() {
+      const { id, name } = this.dataList.reduce((p, v) =>
+        p.id < v.id ? v : p
+      );
       this.dataList.splice(2, 0, {
         id: id + 1,
         name: name + 1,
         lip: true
-      })
+      });
     }
-  },
-}
+  }
+};
 </script>
 
 <style lang="less">
 .index-page {
-  width:100%;
-  height:100%;
-  position:relative;
-  padding:0 0 0 400px;
-  transition:all .3s ease;
-  .tool-box{
-    position:absolute;
-    left:10px;
-    top:50px;
-    .btn{
-      width:200px;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding: 0 0 0 400px;
+  transition: all 0.3s ease;
+  .tool-box {
+    position: absolute;
+    left: 10px;
+    top: 50px;
+    .btn {
+      width: 200px;
       text-align: center;
-      margin-bottom:20px;
-      padding:10px 20px;
-      color:#fff;
-      background-color:#2d8cf0;
-      border-radius:4px;
-      cursor:pointer;
-      transition:all .3s ease;
+      margin-bottom: 20px;
+      padding: 10px 20px;
+      color: #fff;
+      background-color: #2d8cf0;
+      border-radius: 4px;
+      cursor: pointer;
+      transition: all 0.3s ease;
       user-select: none;
-      &:hover{
-        background-color:#57a3f3;
+      &:hover {
+        background-color: #57a3f3;
       }
-      &:focus{
-        box-shadow:0 0 0 2px rgba(45,140,240,.2);
+      &:focus {
+        box-shadow: 0 0 0 2px rgba(45, 140, 240, 0.2);
       }
-      &:active{
-        background-color:#2d8cf0;
+      &:active {
+        background-color: #2d8cf0;
       }
     }
-    .tips{
-      code{
-        background:#000;
-        color:#fff;
-        padding:10px;
-        border-radius:6px;
+    .tips {
+      code {
+        background: #000;
+        color: #fff;
+        padding: 10px;
+        border-radius: 6px;
       }
     }
   }
-  .result-box{
-    width:100%;
-    height:500px;
-    padding-top:100px;
-    transition:all .3s;
-    .vue-drag-select{
-      background-color:#ddd;
+  .result-box {
+    width: 100%;
+    height: 500px;
+    padding-top: 100px;
+    transition: all 0.3s;
+    .vue-drag-select {
+      background-color: #ddd;
     }
     .item-self {
       width: 100%;
@@ -145,11 +157,11 @@ export default {
     }
   }
 }
-.bigger{
-  width:100%;
-  padding:0 0 0 100px;
-  .result-box{
-    height:600px;
+.bigger {
+  width: 100%;
+  padding: 0 0 0 100px;
+  .result-box {
+    height: 600px;
   }
 }
 </style>
